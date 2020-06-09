7c552609869277eaef07cb3ba49c4232921e92f8
DiSheng1992 <shengdi.yz@gmail.com>
2020-06-09 21:01:52 +0100

    add the first imputation function with test

--- data_engineering/cmpny_bc_cls/utils/feature_imputation.py
+++ data_engineering/cmpny_bc_cls/utils/feature_imputation.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+
+def fill_null_with_zeros(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+    df[columns] = df[columns].fillna(0)
+    return df
--- data_engineering/test/test_feature_imputation.py
+++ data_engineering/test/test_feature_imputation.py
@@ -1,1 +1,26 @@
 import pytest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+import numpy as np
+
+from cmpny_bc_cls.utils.feature_imputation import fill_null_with_zeros
+
+
+@pytest.mark.parametrize(
+    "inputs,expected",
+    [
+        (
+            pd.DataFrame(
+                data=np.array([[1, 2, None], [None, 3, 4]], dtype=float),
+                columns=["col1", "col2", "col3"],
+            ),
+            pd.DataFrame(
+                data=np.array([[1, 2, 0], [0, 3, 4]], dtype=float),
+                columns=["col1", "col2", "col3"],
+            ),
+        ),
+    ],
+)
+def test_fill_null_with_zeros(inputs, expected):
+    results = fill_null_with_zeros(inputs, ["col1", "col3"])
+    assert_frame_equal(results, expected)
