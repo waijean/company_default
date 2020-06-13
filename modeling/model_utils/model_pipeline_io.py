4f4e47623a8ea5da05110455db7d1708666c5820
Wai Jean Koh <kwj_97@hotmail.com>
2020-06-13 20:21:17 +0100

    add read cleaned ratio and cleaned raw to model pipeline io

--- modeling/model_utils/model_pipeline_io.py
+++ modeling/model_utils/model_pipeline_io.py
@@ -15,3 +15,19 @@ def read_train_file() -> pd.DataFrame:
     paras = get_para()
     df = pd.read_csv(paras["path_to_clean_train_file"], index_col="company_id")
     return df
+
+
+def read_clean_train_file():
+    paras = get_para()
+    file_path = os.path.join(paras["path_to_output_dir"], "cleaned_ratio_train.csv")
+    print(f"Read cleaned ratio data set from: {file_path}")
+    df = pd.read_csv(file_path, index_col="company_id")
+    return df
+
+
+def read_raw_values_file():
+    paras = get_para()
+    file_path = os.path.join(paras["path_to_output_dir"], "cleaned_raw_train.csv")
+    print(f"Read cleaned raw data set from: {file_path}")
+    df = pd.read_csv(file_path, index_col="company_id")
+    return df
