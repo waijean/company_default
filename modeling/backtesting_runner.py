b40485799253f4e0c969a1c2114a71ce807901ae
Wai Jean Koh <kwj_97@hotmail.com>
2020-10-29 11:15:34 +0000

    refactor backtesting runner

--- modeling/backtesting_runner.py
+++ modeling/backtesting_runner.py
@@ -1,13 +1,20 @@
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import (
-    precision_recall_fscore_support,
     classification_report,
+    precision_score,
+    recall_score,
+    fbeta_score,
 )
 
+from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import make_pipeline
 from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.svm import SVC
 
 from conf.tables import TRAIN_RAW_SET, TRAIN_RATIO_SET, TRAIN_COMBINED_SET
 from modeling.utils import model_pipeline_io
@@ -15,18 +22,7 @@ from modeling.utils import model_pipeline_io
 RANDOM_STATE = 0
 
 
-def run(train_set_name: list):
-    train_data, target = model_pipeline_io.get_training_set(train_set_name)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        train_data, target, test_size=0.2, random_state=RANDOM_STATE
-    )
-
-    pipeline = make_pipeline(
-        QuantileTransformer(),
-        BalancedRandomForestClassifier(random_state=RANDOM_STATE),
-    )
-
+def get_cv_metrics(pipeline, X_train, y_train):
     # Evaluate performance on cross validation set
     cv_metrics = cross_validate(
         pipeline, X_train, y_train, cv=5, scoring=["precision", "recall", "f1"]
@@ -35,34 +31,58 @@ def run(train_set_name: list):
     average_cv_metrics = cv_metrics_df[
         ["test_precision", "test_recall", "test_f1"]
     ].mean()
-    print(f"Metrics on CV set: \n{average_cv_metrics.to_string()}")
+    print(f"Metrics on CV set: \n{average_cv_metrics.to_string()}\n")
+    return cv_metrics_df
 
+
+def get_test_metrics(pipeline, X_test, y_test):
     # Evaluate performance on test set
-    pipeline.fit(X_train, y_train)
     y_pred = pipeline.predict(X_test)
-    (
-        test_precision,
-        test_recall,
-        test_f1_score,
-        test_support,
-    ) = precision_recall_fscore_support(y_test, y_pred, beta=1.0)
     test_metrics = {
-        "precision": test_precision,
-        "recall": test_recall,
-        "f1_score": test_f1_score,
+        "precision": precision_score(y_test, y_pred),
+        "recall": recall_score(y_test, y_pred),
+        "f1_score": fbeta_score(y_test, y_pred, beta=1.0),
     }
     print(f"Metrics on test set: \n{classification_report(y_test, y_pred)}\n")
+    return test_metrics
 
+
+def get_feature_importance(pipeline, X):
     # Get feature importance
     feature_importance = pd.Series(
-        data=pipeline[-1].feature_importances_, index=train_data.columns,
+        data=pipeline[-1].feature_importances_, index=X.columns,
     ).sort_values(ascending=False)
     print(f"Top 10 features: \n{feature_importance[:10].to_string()}\n")
+    return feature_importance
 
-    return cv_metrics_df, test_metrics, feature_importance
+
+def run(X, y, test: bool, feature_importance: bool):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=RANDOM_STATE
+    )
+
+    pipeline = make_pipeline(
+        QuantileTransformer(),
+        RandomUnderSampler(random_state=RANDOM_STATE),
+        GradientBoostingClassifier(random_state=RANDOM_STATE),
+    )
+
+    cv_metrics_df = get_cv_metrics(pipeline, X_train, y_train)
+
+    pipeline.fit(X_train, y_train)
+
+    if feature_importance:
+        feature_importance = get_feature_importance(pipeline, X)
+
+    if test:
+        test_metrics = get_test_metrics(pipeline, X_test, y_test)
+        return cv_metrics_df, feature_importance, test_metrics
+
+    return cv_metrics_df, feature_importance
 
 
 if __name__ == "__main__":
     train_set_name = [TRAIN_COMBINED_SET]
     print(f"Backtesting on {train_set_name} dataset...")
-    cv_metrics_df, test_metrics, feature_importance = run(train_set_name)
+    train_data, target = model_pipeline_io.get_training_set(train_set_name)
+    result = run(train_data, target, test=True, feature_importance=True)
--- modeling/experiments/clustering_runner.py
+++ modeling/experiments/clustering_runner.py
@@ -5,40 +5,48 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.pipeline import make_pipeline
 
+import backtesting_runner
 from conf.tables import TRAIN_COMBINED_SET
 from modeling.utils import model_pipeline_io
+from utils.constants import BANKRUPTCY_LABEL
 
 RANDOM_STATE = 0
 
 
 def run(train_set_name: list):
-    train_data, target = model_pipeline_io.get_training_set(train_set_name)
+    X, y = model_pipeline_io.get_training_set(train_set_name)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        train_data, target, test_size=0.2, random_state=RANDOM_STATE
+        X, y, test_size=0.2, random_state=RANDOM_STATE
     )
 
+    # experiment with different number of clusters
     for n_clusters in range(2, 5):
-        # fit and predict cluster label
         pipeline = make_pipeline(
             QuantileTransformer(),
             KMeans(n_clusters=n_clusters, random_state=RANDOM_STATE),
         )
         pipeline.fit(X_train)
-        X_labels = pipeline.predict(X_train)
 
-        # compare the target label proportion in each cluster
-        df = pd.DataFrame({"cluster_labels": X_labels, "target": y_train})
-        cluster_df = df.groupby(["cluster_labels", "target"]).size()
+        X_with_labels = X.assign(cluster_labels=pipeline.predict(X))
+        compare_clusters(X_with_labels, y, n_clusters)
 
-        cluster_pct_df = cluster_df.groupby(level=0).apply(
-            lambda x: 100 * x / float(x.sum())
+        X_with_dummy_labels = pd.get_dummies(X_with_labels, columns=["cluster_labels"])
+        backtesting_runner.run(
+            X_with_dummy_labels, y, test=False, feature_importance=True
         )
-        combined_df = pd.concat([cluster_df, cluster_pct_df], axis=1)
-        combined_df.columns = ["count", "percentage"]
-        print(
-            f"Clustering result for {n_clusters} clusters: \n{combined_df.to_string()}\n"
-        )
+
+
+def compare_clusters(X, y, n_clusters):
+    # compare the target label proportion in each cluster
+    df = pd.concat([X["cluster_labels"], y], axis=1)
+    cluster_df = df.groupby(["cluster_labels", BANKRUPTCY_LABEL]).size()
+    cluster_pct_df = cluster_df.groupby(level=0).apply(
+        lambda x: 100 * x / float(x.sum())
+    )
+    combined_df = pd.concat([cluster_df, cluster_pct_df], axis=1)
+    combined_df.columns = ["count", "percentage"]
+    print(f"Clustering result for {n_clusters} clusters: \n{combined_df.to_string()}\n")
 
 
 if __name__ == "__main__":
